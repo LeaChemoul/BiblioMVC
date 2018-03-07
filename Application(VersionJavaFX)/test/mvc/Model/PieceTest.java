e2a6b6db934a2915238d31ac9982b99a28f1cdf9
LeaChemoul <34442368+LeaChemoul@users.noreply.github.com>
2018-03-07 21:50:04 +0100

    La classe de Test avec JUnit5
    Voir avec moi pour configurer sur l'IDE.

---
+++ Application(VersionJavaFX)/test/mvc/Model/PieceTest.java
@@ -0,0 +1,24 @@
+package mvc.Model;
+
+import com.sun.javafx.geom.Vec2d;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+
+    @Test
+    void rotation() {
+        PieceBuilder pieceBuilder = new PieceBuilder();
+        Vec2d[] listeVect = new Vec2d[4];
+        listeVect[0] = new Vec2d(2,2);
+        listeVect[1] = new Vec2d(1,2);
+        listeVect[2] = new Vec2d(3,2);
+        listeVect[3] = new Vec2d(1,3);
+        pieceBuilder.addPiece("test",4,4, Color.rgb(120,150,1),2,listeVect);
+        pieceBuilder.afficherPiece("test");
+        pieceBuilder.getPiece("test").Rotation(Direction.RIGHT);
+        pieceBuilder.afficherPiece("test");
+    }
+}
